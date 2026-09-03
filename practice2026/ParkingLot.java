f894cf908897e3c9b099e4049dad13611b8601b6
jagrit29 <jagritbhupal29@gmail.com>
2026-09-03 21:44:16 +0530

    Parking Lot Practice

---
+++ practice2026/ParkingLot.java
@@ -0,0 +1,70 @@
+package practice2026;
+
+/*
+Requirements
+Design PArking lot System 
+1. Parking lot can have multiple levels 
+2. Each parking level can have multiple parking spots 
+3. Parking spot can have be for car, bike, truck (Vehicle type)
+4. 
+
+Core Entities 
+1. ParkingSpot - id, parkingSpotType, vechile
+2. Vehcile -> id, vechileType
+3. ParkingLot -> Level, ParkingSpot[]
+4. Parking
+
+ */
+
+enum VechileType {
+    SMALL,
+    MEDIUM,
+    LARGE
+}
+
+enum SpotType {
+    SMALL, 
+    MEDIUM, 
+    LARGE
+}
+
+class Vechile {
+    String registrationNumber;
+    VechileType vechileType;
+
+    public Vechile(String reg, VechileType vechileType) {
+        this.registrationNumber = reg;
+        this.vechileType = vechileType;
+    }
+}
+
+class ParkingSpot {
+    String id;
+    Vechile parkedVechile;
+    SpotType spotType;
+
+    public ParkingSpot(String id, SpotType spotType) {
+        this.id = id;
+        this.spotType = spotType;
+    }
+
+    public boolean isAvailable() {
+        return parkedVechile == null;
+    }
+
+    public void park(Vechile vechile) {
+        // have a check of vechile type here;
+        // we can have another check for canFIT etc
+        parkedVechile = vechile;
+    }
+
+    public void unpark() {
+        parkedVechile = null;
+    }
+
+}
+
+
+public class ParkingLot {
+    
+}
